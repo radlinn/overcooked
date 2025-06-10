b85a89a4380ca2bcbe798cc9464010b770c16fd4
radlinn <165381728+radlinn@users.noreply.github.com>
2025-06-10 20:36:36 +0200

    files

---
+++ user.js
@@ -0,0 +1,16 @@
+function menuDropdown() {
+    document.getElementById("myDropdownMenu").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
